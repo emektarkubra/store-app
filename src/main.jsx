18bf9e80f50baf2568d7aacbb83809f39032cad3
Kübra Emektar <emektarrkubra@gmail.com>
2023-08-05 23:20:56 +0300

    added ContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import SiteContextProvider from "./context/SiteContext.jsx";
+import SignupContextProvider from "./context/SignupContext.jsx";
+import LoginContextProvider from "./context/LoginContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SiteContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <SignupContextProvider>
+        <LoginContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </LoginContextProvider>
+      </SignupContextProvider>
     </SiteContextProvider>
   </React.StrictMode>
 );
