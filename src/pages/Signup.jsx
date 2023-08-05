19fa7e2c25e3cc9e7fcb7b93cc4b510b01260cef
Kübra Emektar <emektarrkubra@gmail.com>
2023-08-05 23:12:58 +0300

    added Sign up

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,33 @@
+
+import { useContext } from "react";
+import { SignupContext } from "../context/SignupContext";
+import { StyledSignupBox } from "./styled/Signup.styled";
+
+export default function Signup(){
+    
+const {handleChangeSignupInput, handleSignup} = useContext(SignupContext)
+
+    return (
+        <>
+        <StyledSignupBox className = "loginFormBox">
+            <div>Welcome to K-Store! Let’s begin the adventure</div>
+            <form>
+                <div className="inputEmail">
+                    <label htmlFor="inputEmail" className="form-label">Enter your email*</label>
+                    <input onChange={handleChangeSignupInput} type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" name = "email"/>
+                </div>
+                <div className="inputPassword">
+                    <label htmlFor="inputPassword" className="form-label">Create a password*</label>
+                    <input onChange={handleChangeSignupInput} type="password" className="form-control" id="inputPassword" name = "password"/>
+                </div>
+                <div className="inputUserName">
+                    <label htmlFor="inputUserName" className="form-label">Enter a username*</label>
+                    <input onChange={handleChangeSignupInput} type="text" className="form-control" id="inputUserName" name = "userName"/>
+                </div>
+                <button onClick={handleSignup} type="submit" className="btn btn-primary">Sign up</button>
+            </form>
+        </StyledSignupBox>
+        </>
+
+    )
+}
