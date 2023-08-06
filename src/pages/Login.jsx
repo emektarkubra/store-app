09fa858e7aa9335f52f4beebe23c5639653031e5
Kübra Emektar <emektarrkubra@gmail.com>
2023-08-06 23:07:19 +0300

    Added login/feature

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,43 @@
+import { useState } from "react";
 import { useContext } from "react";
-import { LoginContext } from "../context/LoginContext";
+import { StyledAlertBox } from "../components/styled/Alert.styled";
+import { SignContext } from "../context/SignContext";
 import { StyledLoginBox } from "./styled/Login.styled";
+import { BsX } from "react-icons/bs";
+import { useEffect } from "react";
 
-export default function Login(){
 
-    const { handleChangeLoginInput, handleLogin } = useContext(LoginContext)
+export default function Login() {
+
+    const { handleChangeLoginInput, handleLogin, onlineUser } = useContext(SignContext)
+
+
 
     return (
-        <StyledLoginBox className = "loginFormBox">
-            <form>
-                <div className="inputEmail">
-                    <label htmlFor="inputEmail" className="form-label">Email address</label>
-                    <input onChange={handleChangeLoginInput} type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" name = "email"/>
+        <>
+            <StyledLoginBox className="loginBox">
+                {
+                    onlineUser === undefined && <StyledAlertBox>Incorrect username or password. <button> <BsX /></button> </StyledAlertBox>
+
+                }
+                <div className="loginFormBox">
+                    <form onSubmit={handleLogin}>
+                        <div className="inputEmail">
+                            <label htmlFor="inputEmail" className="form-label">Email address</label>
+                            <input onChange={handleChangeLoginInput} type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" name="email" required />
+                        </div>
+                        <div className="inputPassword">
+                            <label htmlFor="inputPassword" className="form-label">Password</label>
+                            <input onChange={handleChangeLoginInput} type="password" className="form-control" id="inputPassword" name="password" required />
+                        </div>
+                        <div className="form-check">
+                            <input onChange={handleChangeLoginInput} type="checkbox" className="form-check-input" id="check" name="check" required />
+                            <label className="form-check-label" htmlFor="check">Check me out</label>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Sign in</button>
+                    </form>
                 </div>
-                <div className="inputPassword">
-                    <label htmlFor="inputPassword" className="form-label">Password</label>
-                    <input onChange={handleChangeLoginInput} type="password" className="form-control" id="inputPassword" name = "password"/>
-                </div>
-                <div className="form-check">
-                    <input onChange={handleChangeLoginInput} type="checkbox" className="form-check-input" id="check" name = "check"/>
-                    <label className="form-check-label" htmlFor="check">Check me out</label>
-                </div>
-                <button onClick={handleLogin} type="submit" className="btn btn-primary">Sign in</button>
-            </form>
-        </StyledLoginBox>
+            </StyledLoginBox>
+        </>
     )
 }
--- src/pages/styled/Login.styled.jsx
+++ src/pages/styled/Login.styled.jsx
@@ -1,45 +1,58 @@
 import { styled } from "styled-components";
 
 export const StyledLoginBox = styled.div`
-border : 1px solid #00000078;
-  width: 290px;
-  margin: auto;
-  margin-top : 10vw;
-  padding: 2vw;
-  border-radius : 6px;
+  margin-top: 5vw;
 
-
-  & > form > .inputEmail,
-  & > form > .inputPassword,
-  & > form > .form-check {
+  & > div > button {
+    background-color: rgb(132, 32, 41);
+    border: none;
     font-size: 14px;
-    margin-bottom: 16px;
-    margin-top : 4px;
-    
-  }
-  
-
-  & > form > div > .form-label {
-    margin-right: 1vw;
-    margin-bottom : 8px;
-    display: block;
-  }
-
-  & > form > div > .form-control {
-    width: 274px;
-    height: 1.5vw;
-    font-size: 1vw;
-  }
-
-  button {
-    width : 282px;
-    background-color: #238636;
-    border: 1px solid rgba(240, 246, 252, 0.1);
-    border-radius : 6px;
-    color: #ffffff;
-    font-size: 1vw;
-    padding: 0.5vw;
-    /* margin-left: 1vw; */
+    color: rgb(44, 11, 14);
     cursor: pointer;
+  }
+
+  & > div > button :hover {
+    color: rgb(230, 237, 243);
+  }
+
+  & > .loginFormBox {
+    border: 1px solid #00000078;
+    width: 290px;
+    margin: auto;
+    margin-top: 10px;
+    padding: 2vw;
+    border-radius: 6px;
+
+    & > form > .inputEmail,
+    & > form > .inputPassword,
+    & > form > .form-check {
+      font-size: 14px;
+      margin-bottom: 16px;
+      margin-top: 4px;
+    }
+
+    & > form > div > .form-label {
+      margin-right: 1vw;
+      margin-bottom: 8px;
+      display: block;
+    }
+
+    & > form > div > .form-control {
+      width: 274px;
+      height: 1.5vw;
+      font-size: 1vw;
+    }
+
+    button {
+      width: 282px;
+      background-color: #238636;
+      border: 1px solid rgba(240, 246, 252, 0.1);
+      border-radius: 6px;
+      color: #ffffff;
+      font-size: 1vw;
+      padding: 0.5vw;
+      /* margin-left: 1vw; */
+      cursor: pointer;
+    }
   }
 `;
